f60a6eadf7e931b36c284c7a8e2f94b4208891b0
Shoabahmad68 <30545090+Shoabahmad68@users.noreply.github.com>
2025-10-27 12:06:46 +0530

    Update Reports.jsx

--- frontend/src/pages/Reports.jsx
+++ frontend/src/pages/Reports.jsx
@@ -15,6 +15,8 @@ import {
 } from "chart.js";
 import { Pie, Bar } from "react-chartjs-2";
 import config from "../config.js";
+import { useData } from "../context/DataContext";
+const { data: excelData, loading } = useData();
 
 ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, Title);
 
