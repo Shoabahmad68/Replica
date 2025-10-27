7094a5306970893eece5a4e9aee9e90c06908a27
Shoabahmad68 <30545090+Shoabahmad68@users.noreply.github.com>
2025-10-27 12:32:37 +0530

    Update Reports.jsx

--- frontend/src/pages/Reports.jsx
+++ frontend/src/pages/Reports.jsx
@@ -21,12 +21,10 @@ import { useData } from "../context/DataContext";
 ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, Title);
 
 export default function Reports() {
-
+  const [data, setData] = useState([]);
   const [message, setMessage] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const rowsPerPage = 20;
-
-const { data: excelData, loading } = useData();
 
   useEffect(() => {
     loadLatestData();
